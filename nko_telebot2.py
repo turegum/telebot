4f96b9d5e6794362bdafb45e001d852501adb7fc
Oleg Zamoshchin <ozamoshin@gmail.com>
2021-10-07 18:46:46 +0300

    pause increased

--- nko_telebot2.py
+++ nko_telebot2.py
@@ -53,7 +53,7 @@ def greeting(message):
 # Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
 
-time.sleep(0.1)
+time.sleep(1)
 
 # Set webhook
 bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
